17059f39bfd20f74c2f47752b0f92d98149d2aae
ask1208 <t.ask.128@icloud.com>
2020-01-05 17:22:33 +0900

    マイグレーション作成

---
+++ database/migrations/2020_01_05_080546_create_posts_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            //unsignedで負の値にならないように設定
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->bigInteger('category_id')->unsigned()->index();
+            
+            $table->string('title')->nullable();
+            $table->text('content')->nullable();
+            
+            //referencesは紐付いている投稿も削除できるように
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
