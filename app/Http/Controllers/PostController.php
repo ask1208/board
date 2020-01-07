6e14261cead6509e7d0a895e9c5d04bdbfc01d8b
ask1208 <t.ask.128@icloud.com>
2020-01-07 21:44:00 +0900

    詳細ボタンとページ作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -49,9 +49,13 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Post $post)
     {
-        //
+        // dd($post);
+        $post->load('category','user');
+        return view('posts.show',[
+            'post' => $post,
+        ]);
     }
 
     /**
